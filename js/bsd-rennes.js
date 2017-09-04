c76f41f1079e064d4d2921d27daa554fe3645789
niamtokik <mk@steepath.eu>
2017-09-04 20:11:18 +0200

    add vue.js side

---
+++ js/bsd-rennes.js
@@ -0,0 +1,22 @@
+var meetup = new Vue({
+  el: '#meetup'
+  data: { 
+    md: "pages/home_meetup.md"
+  }
+  methods: {
+  }
+});
+
+var project = new Vue({
+  el: '#project'
+  data: {
+    md: "pages/home_project.md"
+  }
+});
+
+var event = new Vue({
+  el: '#event'
+  data: {
+    md: "pages/home_projects.md"
+  }
+});
